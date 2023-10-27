f7c86a975f6bc1125f351cfde7a544ad7fccf821
adisharma3107 <as777087@gmail.com>
2023-10-28 03:27:58 +0530

    Commit

---
+++ src/test/java/com/linkedin/kata/RomanTests.java
@@ -0,0 +1,31 @@
+package com.linkedin.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RomanTests {
+    @Test
+    public void junitWorks() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void convertsSingleDigitRoman() {
+        assertEquals(1, Roman.convert("I"));
+        assertEquals(5, Roman.convert("V"));
+        assertEquals(10, Roman.convert("X"));
+        assertEquals(50, Roman.convert("L"));
+        assertEquals(100, Roman.convert("C"));
+        assertEquals(500, Roman.convert("D"));
+        assertEquals(1000, Roman.convert("M"));
+    }
+
+    @Test
+    public void romanNumeralAddition() {
+        assertEquals(2, Roman.convert("II"));
+        assertEquals(3, Roman.convert("III"));
+        assertEquals(6, Roman.convert("VI"));
+    }
+}
